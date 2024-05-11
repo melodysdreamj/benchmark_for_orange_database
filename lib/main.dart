01072d715597061e69232538d04469d38fd35bd2
june lee <melodysdreamj@gmail.com>
2024-05-11 17:48:25 +0900

    `

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:code_timer/code_timer.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:orange/orange.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sembast/sembast.dart';
+import 'package:sembast/sembast_io.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() => runApp(const MyApp());
@@ -43,20 +47,14 @@ class _MyHomePageState extends State<MyHomePage> {
                   Orange.setInt('$i', i);
                 }
                 CodeTimer.stop(label: 'orange 🍊 write performance test');
-              },
-              label: const Text('orange write performance test'),
-            ),
-            const Gap(20),
-            FloatingActionButton.extended(
-              onPressed: () async {
-                await Orange.init();
+
                 CodeTimer.start();
                 for (int i = 0; i < 10000; i++) {
                   Orange.getInt('$i');
                 }
                 CodeTimer.stop(label: 'orange 🍊read performance test');
               },
-              label: const Text('orange read performance test'),
+              label: const Text('orange performance test'),
             ),
             const Gap(20),
             FloatingActionButton.extended(
@@ -67,21 +65,59 @@ class _MyHomePageState extends State<MyHomePage> {
                   await pref.setInt('$i', i);
                 }
                 CodeTimer.stop(label: 'shared_preferences write performance test');
-              },
-              label: const Text('shared_preferences write performance test'),
-            ),
-            const Gap(20),
-            FloatingActionButton.extended(
-              onPressed: () async {
-                SharedPreferences pref = await SharedPreferences.getInstance();
+
                 CodeTimer.start();
                 for (int i = 0; i < 10000; i++) {
                   pref.getInt('$i');
                 }
                 CodeTimer.stop(label: 'shared_preferences read performance test');
               },
-              label: const Text('shared_preferences read performance test'),
+              label: const Text('shared_preferences performance test'),
             ),
+            const Gap(20),
+            FloatingActionButton.extended(
+              onPressed: () async {
+                await Hive.initFlutter();
+                var box = await Hive.openBox('test');
+
+                CodeTimer.start();
+                for (int i = 0; i < 10000; i++) {
+                  await box.put('$i', i);
+                }
+                CodeTimer.stop(label: 'hive write performance test');
+
+                CodeTimer.start();
+                for (int i = 0; i < 10000; i++) {
+                  box.get('$i');
+                }
+                CodeTimer.stop(label: 'hive read performance test');
+              },
+              label: const Text('hive performance test'),
+            ),
+            const Gap(20),
+            //sembast
+            FloatingActionButton.extended(onPressed: () async {
+
+              String dbPath =
+                  '${(await getApplicationDocumentsDirectory()).path}/orange.db';
+              DatabaseFactory dbFactory = databaseFactoryIo;
+
+              var store = StoreRef.main();
+              var db = await dbFactory.openDatabase(dbPath);
+
+              CodeTimer.start();
+              for (int i = 0; i < 10000; i++) {
+                await store.record('$i').put(db, i);
+              }
+              CodeTimer.stop(label: 'sembast write performance test');
+
+              CodeTimer.start();
+              for (int i = 0; i < 10000; i++) {
+                store.record('$i').get(db);
+              }
+              CodeTimer.stop(label: 'sembast read performance test');
+
+            }, label: const Text('sembast performance test')),
           ],
         ),
       ),
